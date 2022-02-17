5f7be67f53a9ee8d545da5ed59629f09900577bd
Vinh NT <vinh.nguyen@thuocsi.vn>
2022-02-17 23:35:17 +0700

    add logout and clearCookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -94,6 +94,8 @@ const authController = {
   },
   logout: async (req, res) => {
     try {
+      res.clearCookie("refreshtoken", { path: "/api/refresh_token" });
+      return res.json({ msg: "logged out" });
     } catch (error) {
       return res.status(500).json({ msg: error.message });
     }
